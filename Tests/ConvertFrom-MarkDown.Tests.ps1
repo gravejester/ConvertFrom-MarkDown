2306b4cd20a1fdde1d9cbbbe60733448e0a807ac
Øyvind Kallstad <kallstad@gmail.com>
2018-04-24 15:50:35 +0200

    add more tests

--- Tests/ConvertFrom-MarkDown.Tests.ps1
+++ Tests/ConvertFrom-MarkDown.Tests.ps1
@@ -21,23 +21,58 @@ Describe "Converting MarkDown" {
     }
 
     It "Converts MarkDown to HTML when ParameterSet is Path" {
-        "# Converting to MarkDown" | Out-File -FilePath .\pestertestfile.md -Force
-        $actual = (ConvertFrom-MarkDown -Path .\pestertestfile.md).Trim()
-        Remove-Item .\pestertestfile.md -Force
-        $actual | Should -Be "<h1>Converting to MarkDown</h1>"
+        $expected = '<h1>Converting to MarkDown</h1>'
+        $actual = (ConvertFrom-MarkDown -Path .\Tests\test.md) -replace('\n','')
+        $actual | Should -Be $expected
     }
 }
 
 Describe "conversion options" {
     It "-UseAbbreviations parameter" {
-        $result = '<p>Later in a text we are using <abbr title="Hypertext Markup Language">HTML</abbr> and it becomes an abbr tag <abbr title="Hypertext Markup Language">HTML</abbr>.</p><p>Later in a text we are using <abbr title="Hypertext Markup Language">HTML</abbr> and it becomes an abbr tag <abbr title="Hypertext Markup Language">HTML</abbr></p>'
+        $expected = '<p>Later in a text we are using <abbr title="Hypertext Markup Language">HTML</abbr> and it becomes an abbr tag <abbr title="Hypertext Markup Language">HTML</abbr>.</p><p>Later in a text we are using <abbr title="Hypertext Markup Language">HTML</abbr> and it becomes an abbr tag <abbr title="Hypertext Markup Language">HTML</abbr></p>'
         $actual = (ConvertFrom-MarkDown -Path .\Tests\abbreviation.md -UseAbbreviations) -replace('\n','')
-        $actual | Should -Be $result
+        $actual | Should -Be $expected
     }
 
     It "-UseAutoIdentifiers parameter" {
-        $result = '<h1 id="this-is-a-heading">This is a heading</h1>'
+        $expected = '<h1 id="this-is-a-heading">This is a heading</h1>'
         $actual = ('# This is a heading' | ConvertFrom-MarkDown -UseAutoIdentifiers) -replace('\n','')
-        $actual | Should -Be $result
+        $actual | Should -Be $expected
+    }
+
+    It "Converts strikethrough correctly using the -UseEmphasisExtras parameter" {
+        $expected = '<p>The following text <del>is deleted</del></p>'
+        $actual = ('The following text ~~is deleted~~' | ConvertFrom-MarkDown -UseEmphasisExtras) -replace('\n','')
+        $actual | Should -Be $expected
+    }
+
+    It "Converts superscript correctly using the  -UseEmphasisExtras parameter" {
+        $expected = '<p>2<sup>10</sup> is 1024</p>'
+        $actual = ('2^10^ is 1024' | ConvertFrom-MarkDown -UseEmphasisExtras) -replace('\n','')
+        $actual | Should -Be $expected
+    }
+
+    It "Converts subscript correctly using the  -UseEmphasisExtras parameter" {
+        $expected = '<p>H<sub>2</sub>O is a liquid</p>'
+        $actual = ('H~2~O is a liquid' | ConvertFrom-MarkDown -UseEmphasisExtras) -replace('\n','')
+        $actual | Should -Be $expected
+    }
+
+    It "Converts inserted correctly using the -UseEmphasisExtras parameter" {
+        $expected = '<p><ins>Inserted text</ins></p>'
+        $actual = ('++Inserted text++' | ConvertFrom-MarkDown -UseEmphasisExtras) -replace('\n','')
+        $actual | Should -Be $expected
+    }
+
+    It "Converts marked correctly using the -UseEmphasisExtras parameter" {
+        $expected = '<p><mark>Marked text</mark></p>'
+        $actual = ('==Marked text==' | ConvertFrom-MarkDown -UseEmphasisExtras) -replace('\n','')
+        $actual | Should -Be $expected
+    }
+
+    It "Converts autolinks correctly using the -UseAutoLinks parameter" {
+        $expected = '<p>This is a <a href="http://www.google.com">http://www.google.com</a> URL and <a href="https://www.google.com">https://www.google.com</a>This is a <a href="ftp://test.com">ftp://test.com</a>And a <a href="mailto:email@toto.com">email@toto.com</a>And a plain <a href="http://www.google.com">www.google.com</a></p>'
+        $actual = (ConvertFrom-MarkDown -Path .\Tests\autolinks.md -UseAutoLinks) -replace('\n','')
+        $actual | Should -Be $expected
     }
 }
---
+++ Tests/autolinks.md
@@ -0,0 +1,4 @@
+This is a http://www.google.com URL and https://www.google.com
+This is a ftp://test.com
+And a mailto:email@toto.com
+And a plain www.google.com
